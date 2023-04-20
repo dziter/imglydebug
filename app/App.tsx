9bf33c777ab90ebc8961ba27ea5e3838b4be6de0
graphee-gabriel <contact.gabriel.morin@gmail.com>
2023-04-20 14:44:25 +0200

    made debug a bit easier

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,41 @@
 import 'expo-dev-client'
 import 'intl'
 import 'intl/locale-data/jsonp/en'
-import React, { FC, memo } from 'react'
+import React, { FC, memo, useCallback, useState } from 'react'
+import { Button, Text, View } from 'react-native'
 import { VideoEditorModal } from 'react-native-videoeditorsdk'
 
 const App: FC<any> = memo(() => {
+  const [showVideoEditorModal, setShowVideoEditorModal] = useState(true)
+  const [message, setMessage] = useState('')
+  const onCancel = useCallback(() => {
+    const errorMessage = 'User cancelled the modal'
+    console.log(errorMessage)
+    setMessage(errorMessage)
+    setShowVideoEditorModal(false)
+  }, [])
+  const onError = useCallback((error: any) => {
+    console.log(error)
+    setMessage(JSON.stringify(error, null, 2))
+    setShowVideoEditorModal(false)
+  }, [])
+  const onExport = useCallback((result: any) => {
+    console.log(result)
+    setMessage(JSON.stringify(result, null, 2))
+    setShowVideoEditorModal(false)
+  }, [])
   return (
-    <VideoEditorModal
-      onCancel={() => console.log('canceled')}
-      onError={(error) => console.log(error)}
-      onExport={(result) => console.log(result)}
-      video={'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4'}
-      visible={true}
-    />
+    <View>
+      <Text>{message}</Text>
+      <Button onPress={() => setShowVideoEditorModal(true)} title={`Show modal`} />
+      <VideoEditorModal
+        onCancel={onCancel}
+        onError={onError}
+        onExport={onExport}
+        video={'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4'}
+        visible={showVideoEditorModal}
+      />
+    </View>
   )
 })
 
