1afa1f25e5941b0e10d19c9c4c5467b4038a5ff3
graphee-gabriel <contact.gabriel.morin@gmail.com>
2023-04-20 15:31:00 +0200

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,11 @@ import 'expo-dev-client'
 import 'intl'
 import 'intl/locale-data/jsonp/en'
 import React, { FC, memo, useCallback, useState } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { VideoEditorModal } from 'react-native-videoeditorsdk'
 
 const App: FC<any> = memo(() => {
-  const [showVideoEditorModal, setShowVideoEditorModal] = useState(true)
+  const [showVideoEditorModal, setShowVideoEditorModal] = useState(false)
   const [message, setMessage] = useState('')
   const onCancel = useCallback(() => {
     const errorMessage = 'User cancelled the modal'
@@ -24,10 +24,16 @@ const App: FC<any> = memo(() => {
     setMessage(JSON.stringify(result, null, 2))
     setShowVideoEditorModal(false)
   }, [])
+  const onPressShowModal = useCallback(() => {
+    setShowVideoEditorModal(true)
+    setMessage('')
+  }, [])
   return (
-    <View>
-      <Text>{message}</Text>
-      <Button onPress={() => setShowVideoEditorModal(true)} title={`Show modal`} />
+    <View style={s.viewMain}>
+      <View style={s.viewContainer}>
+        <Text>{message}</Text>
+        <Button onPress={onPressShowModal} title={`Show modal`} />
+      </View>
       <VideoEditorModal
         onCancel={onCancel}
         onError={onError}
@@ -39,4 +45,14 @@ const App: FC<any> = memo(() => {
   )
 })
 
+const s = StyleSheet.create({
+  viewContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 32,
+    paddingVertical: 64,
+  },
+  viewMain: { flex: 1 },
+})
 export default App
